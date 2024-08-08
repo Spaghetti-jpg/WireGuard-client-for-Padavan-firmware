c8019e5e62cbcab1363b567d255c4c1e5ef25894
Spaghetti-jpg <77028114+Spaghetti-jpg@users.noreply.github.com>
2024-08-09 01:08:18 +0800

    Update wg-client.sh

--- wg-client.sh
+++ wg-client.sh
@@ -85,6 +85,12 @@ start() {
   resolve_and_update_ipset
 
   modprobe wireguard
+  modprobe ip_set
+  modprobe ip_set_hash_ip
+  modprobe ip_set_hash_net
+  modprobe ip_set_bitmap_ip
+  modprobe ip_set_list_set
+  modprobe xt_set
   ip link add dev $IFACE type wireguard
   ip addr add $WG_CLIENT/$WG_MASK dev $IFACE
   wg setconf $IFACE "$WG_CONFIG"
