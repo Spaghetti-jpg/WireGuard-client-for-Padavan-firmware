4efa0f68b4f772d4b8f16777a65b82f3f1cf6a28
Spaghetti-jpg <77028114+Spaghetti-jpg@users.noreply.github.com>
2024-10-02 22:25:04 +0800

    feat: Extended CIDR support and improved script functionality
    - Added support for CIDR management, lists are stored in config/CIDR.lst.
    - Replaced domains.txt file with config/domains.lst for more structured file storage.
    - Implemented ipset backup every 3 hours (10800 seconds), backups are stored in config directory.
    - Added IPSET_BACKUP variable to control the backup function, default value is false.
    - Created config directory for convenient storage of configuration files.
    - Removed unnecessary kernel modules: ip_set_hash_ip, ip_set_bitmap_ip, ip_set_list_set, xt_set to simplify work.
    - Changed path of unblock.dnsmasq file to /tmp to save flash memory.
    - Various minor improvements to improve performance and usability of the script.

--- wg-client.sh
+++ wg-client.sh
@@ -4,16 +4,22 @@ IFACE="wg0"
 WG_SERVER="10.7.0.1"
 WG_CLIENT="10.7.0.2"
 WG_MASK="32"
+MTU="1480"
 WG_CONFIG="wg0.conf"
 IPSET_NAME="unblock-list"
+
 IPSET_TIMEOUT="43200"
-MTU="1280"
-DOMAINS_FILE="domains.txt"
-DNSMASQ_FILE="unblock.dnsmasq"
-SYSLOG_FILE="/tmp/syslog.log"
 COMMENT_UPDATE_INTERVAL=20
-DOMAINS_UPDATE_INTERVAL=21600
+DOMAINS_UPDATE_INTERVAL=10800
+
+
+DOMAINS_FILE="config/domains.lst"
+CIDR_FILE="config/CIDR.lst"
+DNSMASQ_FILE="/tmp/unblock.dnsmasq"
+SYSLOG_FILE="/tmp/syslog.log"
 PID_FILE="/tmp/update_ipset.pid"
+IPSET_BACKUP_FILE="config/ipset_backup.conf"
+IPSET_BACKUP="false" #true/false
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -26,13 +32,27 @@ log() {
 
 create_ipset() {
   if ! ipset list $IPSET_NAME > /dev/null 2>&1; then
-    log "Creating ipset $IPSET_NAME with timeout and comments..."
+    log "Creating ipset $IPSET_NAME with timeout and comments..." $GREEN
     ipset create $IPSET_NAME hash:net comment timeout $IPSET_TIMEOUT
   fi
 }
 
+restore_ipset() {
+  if [ -f "$IPSET_BACKUP_FILE" ]; then
+    ipset restore -exist -f "$IPSET_BACKUP_FILE"
+    log "Ipset $IPSET_NAME restored from $IPSET_BACKUP_FILE." $GREEN
+  fi
+}
+
+save_ipset() {
+  if [ "$IPSET_BACKUP" = "true" ]; then
+    ipset save $IPSET_NAME > "$IPSET_BACKUP_FILE"
+    log "Ipset $IPSET_NAME saved to $IPSET_BACKUP_FILE." $GREEN
+  fi
+}
+
 resolve_and_update_ipset() {
-  log "Resolving domains and updating ipset $IPSET_NAME..."
+  log "Resolving domains and updating ipset $IPSET_NAME...\n"
 
   if [ ! -f "$DOMAINS_FILE" ]; then
     log "Error: File with unblockable resources $DOMAINS_FILE not found!" $RED
@@ -41,25 +61,49 @@ resolve_and_update_ipset() {
 
   : > $DNSMASQ_FILE
 
+  resolve_domain() {
+    local domain=$1
+    ADDR=$(nslookup $domain localhost | awk '/Address [0-9]+: / {ip=$3} /Address: / {ip=$2} ip ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ && ip != "127.0.0.1" {print ip}')
+
+    if [ -n "$ADDR" ]; then
+      for IP_HOST in $ADDR; do
+        ipset -exist add $IPSET_NAME $IP_HOST timeout $IPSET_TIMEOUT comment "$domain"
+      done
+    fi
+    printf "ipset=/%s/%s\n" "$domain" "$IPSET_NAME" >> $DNSMASQ_FILE
+  }
+
   while read -r line || [ -n "$line" ]; do
     [ -z "$line" ] && continue
     [ "${line:0:1}" = "#" ] && continue
 
-    ADDR=$(nslookup $line localhost | awk '/Address [0-9]+: / {ip=$3} /Address: / {ip=$2} ip ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ && ip != "127.0.0.1" {print ip}')
-
-    if [ -n "$ADDR" ]; then
-      for IP_HOST in $ADDR; do
-        ipset -exist add $IPSET_NAME $IP_HOST timeout $IPSET_TIMEOUT comment "$line"
-      done
-    fi
-    printf "ipset=/%s/%s\n" "$line" "$IPSET_NAME" >> $DNSMASQ_FILE
+    resolve_domain "$line" &
   done < $DOMAINS_FILE
 
-  log "ipset $IPSET_NAME updated."
-  log "dnsmasq config file $DNSMASQ_FILE updated."
+  wait
+
+  log "\nIpset $IPSET_NAME updated."
+  log "Dnsmasq config file $DNSMASQ_FILE updated."
   log "Sending HUP signal to dnsmasq..."
   killall -HUP dnsmasq
 }
+
+update_ipset_from_cidr() {
+  log "Adding CIDR to ipset $IPSET_NAME from $CIDR_FILE file..."
+
+  if [ ! -f "$CIDR_FILE" ]; then
+    log "Warning: CIDR file $CIDR_FILE not found!" $RED
+    return
+  fi
+
+  while read -r cidr || [ -n "$cidr" ]; do
+    [ -z "$cidr" ] && continue
+    ipset -exist add $IPSET_NAME $cidr timeout $IPSET_TIMEOUT comment "$cidr"
+  done < $CIDR_FILE
+
+  log "Ipset $IPSET_NAME updated with CIDR ranges."
+}
+
 
 update_ipset_from_syslog() {
   ipset list $IPSET_NAME | grep '^ *[0-9]' | grep -v 'comment' | awk '{print $1}' | while read -r ip; do
@@ -87,11 +131,7 @@ start() {
 
   modprobe wireguard
   modprobe ip_set
-  modprobe ip_set_hash_ip
   modprobe ip_set_hash_net
-  modprobe ip_set_bitmap_ip
-  modprobe ip_set_list_set
-  modprobe xt_set
 
   if [ ! -f "$WG_CONFIG" ]; then
     log "Error: WireGuard config file $WG_CONFIG not found!" $RED
@@ -99,7 +139,9 @@ start() {
   fi
 
   create_ipset
+  restore_ipset
   resolve_and_update_ipset
+  update_ipset_from_cidr
 
   ip link add dev $IFACE type wireguard
   ip addr add $WG_CLIENT/$WG_MASK dev $IFACE
@@ -127,17 +169,23 @@ start() {
       wait $child_pid
       remove_pid $child_pid
     done ) &
+
   echo $! >> $PID_FILE
 
   ( while true; do
-      update
+      save_ipset
       sleep $DOMAINS_UPDATE_INTERVAL &
       child_pid=$!
       echo $child_pid >> $PID_FILE
       wait $child_pid
       remove_pid $child_pid
     done ) &
+
   echo $! >> $PID_FILE
+
+  if [ "$IPSET_BACKUP" != "true" ]; then
+    log "Skipping ipset backup as IPSET_BACKUP is not true." $RED
+  fi
 }
 
 stop() {
@@ -167,8 +215,10 @@ stop() {
 update() {
   if [ -f "$PID_FILE" ]; then
     resolve_and_update_ipset >/dev/null 2>&1
+    update_ipset_from_cidr >/dev/null 2>&1
   else
     resolve_and_update_ipset
+    update_ipset_from_cidr
   fi
 }
 
@@ -177,12 +227,9 @@ clean() {
 
   if ipset list $IPSET_NAME > /dev/null 2>&1; then
     ipset flush $IPSET_NAME
-    log "Ipset set $IPSET_NAME successfully flushed."
-  else
-    log "Ipset set $IPSET_NAME does not exist."
+    log "Ipset set $IPSET_NAME cleaned."
   fi
 }
-
 
 trap 'log "Script interrupted, cleaning up..."; stop; exit 1' INT TERM
 
